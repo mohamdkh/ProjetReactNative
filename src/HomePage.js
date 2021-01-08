9d96c28ab27404cce67cc239bff24fad20460578
chaimae-lm <chaimaelemtelet@gmail.com>
2021-01-08 19:53:01 +0100

    login_register_home_welcome

---
+++ src/HomePage.js
@@ -0,0 +1,165 @@
+import React from 'react'
+import Swiper from 'react-native-swiper'
+import {
+    View,
+    Text,
+    Image,
+    StyleSheet,
+    StatusBar,
+    TouchableOpacity,
+    ScrollView,
+  } from 'react-native';
+const HomePage = (props) => {
+    return (
+        <ScrollView style={styles.container}>
+            <View style={styles.sliderContainer}>
+            <Swiper
+          autoplay
+          horizontal={false}
+          height={200}
+          activeDotColor="#FF6347">
+          <View style={styles.slide}>
+            <Image
+              source={require('../assets/img1.jpg')}
+              resizeMode="cover"
+              style={styles.sliderImage}
+            />
+          </View>
+          <View style={styles.slide}>
+            <Image
+              source={require('../assets/img2.png')}
+              resizeMode="cover"
+              style={styles.sliderImage}
+            />
+          </View>
+          <View style={styles.slide}>
+            <Image
+              source={require('../assets/img3.jpg')}
+              resizeMode="cover"
+              style={styles.sliderImage}
+            />
+          </View>
+          <View style={styles.slide}>
+            <Image
+              source={require('../assets/img4.jpg')}
+              resizeMode="cover"
+              style={styles.sliderImage}
+            />
+          </View>
+        </Swiper>
+            </View>
+            <View style={styles.cardsWrapper}>
+        <Text
+          style={{
+            alignSelf: 'center',
+            fontSize: 18,
+            fontWeight: 'bold',
+            color: '#333',
+          }}>
+            A propos de l'appli
+        </Text>
+        </View>
+        </ScrollView>
+
+    )
+}
+export default HomePage;
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+    },
+    sliderContainer: {
+        height: 200,
+        width: '90%',
+        marginTop: 10,
+        justifyContent: 'center',
+        alignSelf: 'center',
+        borderRadius: 8,
+    },
+
+    wrapper: {},
+
+    slide: {
+        flex: 1,
+        justifyContent: 'center',
+        backgroundColor: 'transparent',
+        borderRadius: 8,
+    },
+    sliderImage: {
+        height: '100%',
+        width: '100%',
+        alignSelf: 'center',
+        borderRadius: 8,
+    },
+    categoryContainer: {
+        flexDirection: 'row',
+        width: '90%',
+        alignSelf: 'center',
+        marginTop: 25,
+        marginBottom: 10,
+    },
+    categoryBtn: {
+        flex: 1,
+        width: '30%',
+        marginHorizontal: 0,
+        alignSelf: 'center',
+    },
+    categoryIcon: {
+        borderWidth: 0,
+        alignItems: 'center',
+        justifyContent: 'center',
+        alignSelf: 'center',
+        width: 70,
+        height: 70,
+        backgroundColor: '#fdeae7' /* '#FF6347' */,
+        borderRadius: 50,
+    },
+    categoryBtnTxt: {
+        alignSelf: 'center',
+        marginTop: 5,
+        color: '#de4f35',
+    },
+    cardsWrapper: {
+        marginTop: 20,
+        width: '90%',
+        alignSelf: 'center',
+    },
+    card: {
+        height: 100,
+        marginVertical: 10,
+        flexDirection: 'row',
+        shadowColor: '#999',
+        shadowOffset: { width: 0, height: 1 },
+        shadowOpacity: 0.8,
+        shadowRadius: 2,
+        elevation: 5,
+    },
+    cardImgWrapper: {
+        flex: 1,
+    },
+    cardImg: {
+        height: '100%',
+        width: '100%',
+        alignSelf: 'center',
+        borderRadius: 8,
+        borderBottomRightRadius: 0,
+        borderTopRightRadius: 0,
+    },
+    cardInfo: {
+        flex: 2,
+        padding: 10,
+        borderColor: '#ccc',
+        borderWidth: 1,
+        borderLeftWidth: 0,
+        borderBottomRightRadius: 8,
+        borderTopRightRadius: 8,
+        backgroundColor: '#fff',
+    },
+    cardTitle: {
+        fontWeight: 'bold',
+    },
+    cardDetails: {
+        fontSize: 12,
+        color: '#444',
+    },
+});
