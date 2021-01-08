85c7a0548cf5d47358972f061373a2ac53d19d78
mohamdkh <mohamedkhafchrait@gmail.com>
2021-01-08 17:30:52 +0100

    Init project

---
+++ App.js
@@ -0,0 +1,13 @@
+import * as React from 'react';
+import { View, ActivityIndicator,Alert } from 'react-native';
+const Drawer = createDrawerNavigator();
+// screen 
+
+export default function App() {
+  
+  return (
+	<View>
+	<Text>Hellooo</Text>
+	</View>
+  );
+}
